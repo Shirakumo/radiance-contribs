6ddbfdbe50ee0c6f241c85e3edd7340170f9e5f2
Shinmera <shinmera@tymoon.eu>
2014-12-27 19:23:47 +0100

    Fix for radiance-framework internal domain handling, merged files together since they're small enough.

--- i-wookie/i-wookie.asd
+++ i-wookie/i-wookie.asd
@@ -12,9 +12,7 @@
   :maintainer "Nicolas Hafner <shinmera@tymoon.eu>"
   :version "1.0.1"
   :description "A bridging library to allow using the Wookie webserver as Radiance's server implementation."
-  :components ((:file "module")
-               (:file "toolkit")
-               (:file "server"))
+  :components ((:file "server"))
   :depends-on (:wookie
                :cl-ppcre
                :bordeaux-threads
--- i-wookie/server.lisp
+++ i-wookie/server.lisp
@@ -4,7 +4,14 @@
  Author: Nicolas Hafner <shinmera@tymoon.eu>
 |#
 
-(in-package #:i-wookie)
+(in-package #:modularize-user)
+(define-module #:i-wookie
+  (:use #:cl #:radiance)
+  (:implements #:server))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (setf wookie:*enabled-plugins* '(:get :post :cookie :multipart))
+  (wookie:load-plugins))
 
 (defvar *listeners* (make-hash-table :test 'equalp))
 (defvar *listener-lock* (bt:make-lock "LISTENERS"))
@@ -56,9 +63,42 @@
     (loop for name being the hash-keys of *listeners*
           collect name)))
 
-(defun handle-request (request wk-response)
-  (l:trace :server "Handle: ~a" request)
-  (let ((response (execute-request request)))
+(defun whenthen (var func)
+  (when var (funcall func var)))
+
+(defun merge-hash-tables (target source &optional (transform #'identity))
+  (when (and source target)
+    (maphash #'(lambda (k v) (setf (gethash k target) (funcall transform v))) source))
+  target)
+
+(defun set-real-cookie (cookie wk-response)
+  (wookie-plugin-export:set-cookie
+   wk-response
+   (name cookie)
+   (value cookie)
+   :expires (expires cookie)
+   :path (path cookie)
+   :domain (domain cookie)
+   :secure (secure cookie)
+   :http-only (http-only cookie)))
+
+(defun handle-request (wk-request wk-response)
+  (let ((response (request (parse-uri (format NIL "~a~a"
+                                              (gethash "host" (headers request))
+                                              (urlencode:urldecode
+                                               (subseq (wookie:request-resource wk-request) 0
+                                                       (position #\? (wookie:request-resource wk-request)))
+                                               :lenientp T)))
+                           :http-method (wookie:request-method wk-request)
+                           :headers (wookie:request-headers wk-request)
+                           :post (cond ((search "application/x-www-form-urlencoded" (gethash "content-type" (headers request)))
+                                        (wookie:plugin-request-data :post wk-request))
+                                       ((search "multipart/form-data" (gethash "content-type" (headers request)))
+                                        (merge-hash-tables (getf (wookie:plugin-request-data :multipart wk-request) :hash-file)
+                                                           (getf (wookie:plugin-request-data :multipart wk-request) :hash-form))))
+                           :get (wookie:plugin-request-data :get wk-request)
+                           :cookies (wookie:plugin-request-data :cookie wk-request)
+                           :remote "unknown")))
     (handler-bind ((error #'handle-condition))
       ;; Process attributes
       (maphash #'(lambda (key val) (declare (ignore key)) (set-real-cookie val wk-response)) (cookies response))
@@ -80,7 +120,7 @@
         ((or string (array (unsigned-byte 8)))
          (wookie:send-response wk-response :status (return-code response) :body (data response)))
 
-        (null (error 'request-empty :request request)))
+        (null (error 'request-empty :request NIL)))
       wk-response)))
 
 (wookie:defroute (:* ".*") (req res)
@@ -90,7 +130,7 @@
                               (t (l:severe :server "Error at lowest level: ~a" err)
                                  (invoke-restart 'error-out))))))
     (restart-case 
-        (handle-request (create-real-request req) res)
+        (handle-request req res)
       (error-out ()
         :report "Just error out ungracefully."
         (wookie:send-response res :status 500 :body "Something went very, very wrong.")))))
