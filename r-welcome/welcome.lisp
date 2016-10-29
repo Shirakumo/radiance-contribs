2875abdc6fe26f0224bd798876f641db24f2c2b1
Shinmera <shinmera@tymoon.eu>
2016-10-29 12:14:16 +0200

    Added output message on load to introduce people.

--- r-welcome/welcome.lisp
+++ r-welcome/welcome.lisp
@@ -49,3 +49,19 @@
 
 (define-page internal-error "/500" ()
   (error 'internal-error))
+
+(format T "~&~%
+;;;; You are loading R-WELCOME
+;; This most likely means that this is your first time running
+;; Radiance. Welcome! Once the loading has finished please visit
+;;
+;;    http://localhost:8080/
+;;
+;; with your browser to see a demo page of a running system. If
+;; you would like to get rid of this module, either edit the
+;; configuration file for the ~a environment at
+;; ~a
+;; or start Radiance with a different environment, and thus your
+;; own configuration entirely.
+~%"
+        (environment) (uiop:native-namestring ubiquitous:*storage-pathname*))
