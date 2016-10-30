6998b01243bb24c70d3b7f9e7c054281cce1ba94
Shinmera <shinmera@tymoon.eu>
2016-10-30 11:55:52 +0100

    Print condition reason on error pages.

--- r-simple-errors/error.lisp
+++ r-simple-errors/error.lisp
@@ -9,11 +9,14 @@
   (:use #:cl #:radiance))
 (in-package #:simple-errors)
 
-(defun process (template &rest args)
+(defun process (template condition)
   (plump:serialize
-   (apply #'r-clip:process
-          (plump:parse (template template))
-          args)
+   (r-clip:process
+    (plump:parse (template template))
+    :condition condition
+    :reason (or (when (typep condition 'radiance-error)
+                  (message condition))
+                "Unfortunately, no detailed reason for this problem is available."))
    NIL))
 
 (defun radiance:render-error-page (condition)
@@ -23,13 +26,13 @@
      "")
     (request-not-found
      (setf (return-code *response*) 404)
-     (process "not-found.ctml"))
+     (process "not-found.ctml" condition))
     (request-denied
      (setf (return-code *response*) 403)
      (cond ((and (auth:implementation) (not (auth:current)))
-            (process "login-required.ctml"))
+            (process "login-required.ctml" condition))
            (T
-            (process "access-denied.ctml"))))
+            (process "access-denied.ctml" condition))))
     (error
      (setf (return-code *response*) 500)
-     (process "internal-error.ctml"))))
+     (process "internal-error.ctml" condition))))
