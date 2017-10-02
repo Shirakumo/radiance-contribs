498d0f4163df2fdf288cc7485d089c79c18c296f
Shinmera <shinmera@tymoon.eu>
2017-10-02 12:09:32 +0200

    Fix ensure-collection name for Windows.
    I would've preferred to change the colons to something else everywhere, but alas that would break existing systems on Linux. So the choice is either make a Windows exception, implement upgrading semantics, or allow bot hat the same time. The latter two would incur a lot more lambdalite hacks, which I'd rather not commit to.

--- i-lambdalite/database.lisp
+++ i-lambdalite/database.lisp
@@ -40,7 +40,10 @@
     (string
      (unless (valid-name-p thing)
        (error 'db:invalid-collection :collection thing))
-     (intern (map 'string (lambda (c) (if (char= c #\/) #\: c)) thing) :keyword))
+     (intern (map 'string (lambda (c) (if (char= c #\/)
+                                          #+windows #\;
+                                          #-windows #\:
+                                          c)) thing) :keyword))
     (symbol (ensure-collection
              (format NIL "~a/~a"
                      (package-name (symbol-package thing)) (symbol-name thing))))))
