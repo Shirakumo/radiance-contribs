ece6c743da2627efc4c9f09f9e09ad1007ecfaf5
Shinmera <shinmera@tymoon.eu>
2017-10-25 13:22:59 +0200

    Update r-simple-cache for new interface.

--- r-simple-cache/cache.lisp
+++ r-simple-cache/cache.lisp
@@ -26,23 +26,24 @@
 (defun cache::builder (name)
   (gethash name *caches*))
 
-(defun cache::file (symbol)
-  (merge-pathnames (format NIL "~a/~a" (package-name (symbol-package symbol)) (symbol-name symbol)) *cache-directory*))
+(defun cache::file (symbol &optional variant)
+  (merge-pathnames (format NIL "~a/~a~@[/~a~].cache" (package-name (symbol-package symbol)) (symbol-name symbol) variant)
+                   *cache-directory*))
 
-(defun cache:get (name)
-  (when (cache::exists name)
+(defun cache::exists (name &optional variant)
+  (probe-file (cache::file name variant)))
+
+(defun cache:get (name &optional variant)
+  (when (cache::exists name variant)
     (alexandria:read-file-into-byte-vector
-     (cache::file name))))
+     (cache::file name variant))))
 
-(defun cache:renew (name)
-  (when (cache::exists name)
-    (delete-file (cache::file name))))
+(defun cache:renew (name &optional variant)
+  (when (cache::exists name variant)
+    (delete-file (cache::file name variant))))
 
-(defun cache::exists (name)
-  (probe-file (cache::file name)))
-
-(defun cache::output (name result)
-  (let ((file (cache::file name)))
+(defun cache::output (result name &optional variant)
+  (let ((file (cache::file name variant)))
     (ensure-directories-exist file)
     (etypecase result
       (string
@@ -53,10 +54,12 @@
          (write-sequence result stream)))))
   result)
 
-(defmacro cache:with-cache (name test &body request-generator)
-  (let ((cache (gensym "CACHE")))
-    `(let ((,cache ,name))
-       (if (or (not (cache::exists ,cache))
-               ,test)
-           (cache::output ,cache ((lambda () ,@request-generator)))
-           (cache:get ,cache)))))
+(defmacro cache:with-cache ((name &optional variant) test &body request-generator)
+  (let ((thunk (gensym "THUNK")) (variantg (gensym "VARIANT")))
+    `(flet ((,thunk ()
+              ,@request-generator))
+       (let ((,variantg ,variant))
+         (if ,test
+             (cache::output (,thunk) ',name ,variantg)
+             (or (cache:get ',name ,variantg)
+                 (cache::output (,thunk) ',name ,variantg)))))))
